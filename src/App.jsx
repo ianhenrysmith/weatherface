36dbf5f15e786d64758925f9aa5ea66d2320e845
Ian Smith <ianhenrysmith@gmail.com>
2023-03-27 15:37:08 +0000

    store location in local storage, add button for clearing, update title

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,73 @@ const longitude = '-105.027268';
 const units = 'us';
 
 function App() {
+  const [location, setLocation] = useState({ latitude: null, longitude: null });
   const [weatherData, setWeatherData] = useState(null);
 
+  const getUserLocation = () => {
+    const success = (position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      
+      setLocation({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+
+      localStorage.setItem('latitude', latitude);
+      localStorage.setItem('longitude', longitude);
+    };
+  
+    const error = () => {
+      console.error('Unable to retrieve your location');
+    };
+  
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+      console.error('Geolocation is not supported by your browser');
+    }
+  };
+
+  const fetchWeatherData = () => {
+     fetch(`https://api.pirateweather.net/forecast/${apiKey}/${location.latitude},${location.longitude}?units=${units}&extend=daily`)
+        .then((response) => response.json())
+        .then((data) => setWeatherData(data))
+        .catch((error) => console.error('Error fetching weather data:', error));
+  }
+
+  const clearSavedLocation = () => {
+    // Clear location from localStorage
+    localStorage.removeItem('latitude');
+    localStorage.removeItem('longitude');
+  
+    // Clear location from state
+    setLocation({ latitude: null, longitude: null });
+  
+    // Fetch the user's current location again
+    getUserLocation();
+  };
+
   useEffect(() => {
-    fetch(`https://api.pirateweather.net/forecast/${apiKey}/${latitude},${longitude}?units=${units}&extend=daily`)
-      .then((response) => response.json())
-      .then((data) => setWeatherData(data))
-      .catch((error) => console.error('Error fetching weather data:', error));
+    const savedLatitude = localStorage.getItem('latitude');
+    const savedLongitude = localStorage.getItem('longitude');
+  
+    if (savedLatitude && savedLongitude) {
+      setLocation({ latitude: parseFloat(savedLatitude), longitude: parseFloat(savedLongitude) });
+    } else {
+      getUserLocation();
+    }
   }, []);
+  
+  useEffect(() => {
+    if (location.latitude && location.longitude) {
+      fetchWeatherData(location.latitude, location.longitude);
+    }
+  }, [location]);
 
   return (
     <Container>
-      <h1 className="text-center my-4">Weather App</h1>
+      <h1 className="text-center my-4">Weatherface, the stupid simple weather app</h1>
       <Row>
         <Col>
           {weatherData ? (
@@ -35,6 +90,13 @@ function App() {
           ) : (
             <p>Loading weather data...</p>
           )}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div className="clear-location-button">
+            <button onClick={clearSavedLocation}>Clear Saved Location</button>
+          </div>
         </Col>
       </Row>
     </Container>
